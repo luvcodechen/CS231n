05e9d4424d49a0d7546cce4bc596e73b699938af
luvcodechen <chen_ney@qq.com>
2023-12-27 19:39:33 +0800

    KNN

--- assignment1/cs231n/classifiers/k_nearest_neighbor.py
+++ assignment1/cs231n/classifiers/k_nearest_neighbor.py
@@ -1,3 +1,4 @@
+import math
 from builtins import range
 from builtins import object
 import numpy as np
@@ -76,8 +77,7 @@ class KNearestNeighbor(object):
                 # not use a loop over dimension, nor use np.linalg.norm().          #
                 #####################################################################
                 # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
-                pass
+                dists[i][j] = np.sqrt(np.sum((X[i] - self.X_train[j]) ** 2))
 
                 # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         return dists
@@ -100,8 +100,7 @@ class KNearestNeighbor(object):
             # Do not use np.linalg.norm().                                        #
             #######################################################################
             # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
-            pass
+            dists[i] = np.sqrt(np.sum((X[i] - self.X_train) ** 2, axis=1))
 
             # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         return dists
@@ -131,7 +130,10 @@ class KNearestNeighbor(object):
         #########################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-        pass
+        dists = X.dot(self.X_train.T)
+        X_square = np.square(X).sum(axis=1)
+        X_train_square = np.sum(np.square(self.X_train), axis=1)
+        dists = np.sqrt(X_square.reshape(-1, 1) + X_train_square - dists * 2)
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         return dists
@@ -163,8 +165,7 @@ class KNearestNeighbor(object):
             # Hint: Look up the function numpy.argsort.                             #
             #########################################################################
             # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
-            pass
+            closest_y = self.y_train[np.argsort(dists[i])][:k]
 
             # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
             #########################################################################
@@ -176,7 +177,7 @@ class KNearestNeighbor(object):
             #########################################################################
             # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-            pass
+            y_pred[i] = max(closest_y, key=list(closest_y).count)
 
             # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
--- main.py
+++ main.py
@@ -9,8 +9,28 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+img = np.array(Image.open('cat.jpg'))
+img_tinted = img * [1, 0.95, 0.9]
+
+# Show the original image
+plt.subplot(1, 2, 1)
+plt.imshow(img)
+
+# Show the tinted image
+plt.subplot(1, 2, 2)
+
+# A slight gotcha with imshow is that it might give strange results
+# if presented with data that is not uint8. To work around this, we
+# explicitly cast the image to uint8 before displaying it.
+plt.imshow(np.uint8(img_tinted))
+plt.show()
+
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
